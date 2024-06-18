b5b02aea3d56b9bad397e209112668f7d99af5dc
Zippy-Waluse <zippywaluse@gmail.com>
2024-06-18 09:25:48 +0300

    added Mission

---
+++ econtrust/src/mission/index.js
@@ -0,0 +1,14 @@
+import './Mision.css';
+const Mission=()=>{
+  return(
+             <div className='textbox'>
+               <h1 className='mission-text'> Our Mission</h1>
+               <p className='text'>We aim to support Rwandan tea farmers by boosting their<br></br>
+               inancial strength through VSLAs.Our goals include<br></br>
+               enhancing saety,streamlining paperwork,lowering <br></br>
+              expenses,and offering digital ways to save by utilizing the <br></br>VSLAs strengths</p>
+           </div>
+       )
+}
+
+export default Mission;

db5b57706fac0e765e739199d6c09e21c9d74c06
Mary-Vivian <muthonimaryvivian>
2024-06-18 15:42:21 +0300

    added media queries

--- econtrust/src/About/index.js
+++ econtrust/src/About/index.js
@@ -1,13 +1,17 @@
 import './index.css';
 function About(){
     return(
-        <div id="aboutus">
+        <>
+       <div>
             <h1>ABOUT US</h1>
-            <p id="about">EconTrust is a product developed by Samaa group,the
+            </div>
+            <div id="aboutus">
+            <p>EconTrust is a product developed by Samaa group,the
                 product is developed with a human centered approach thinking
                 to enable users to save and track their savings
                 and set goals on their saving </p>
         </div>
+        </>
     )
 }
 export default About;

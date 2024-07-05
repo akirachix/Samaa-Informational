0b5fbb685d03cd6c84e9b69c8cb033fe60d1a796
Mary-Vivian <muthonimaryvivian>
2024-07-05 18:41:59 +0300

    made changes

---
+++ econtrust/src/About/index.js
@@ -0,0 +1,22 @@
+import './index.css';
+
+function About(){
+    return(
+        <>
+       <div>
+            <h1>About Us</h1>
+            </div>
+            <div id="aboutus">
+            <p>EconTrust, is an innovative financial tool
+            developed by the Samaa Group, it embodies a human-centered design approach. 
+            This approach ensures that the product is not just technologically advanced but also deeply
+            intuitive and user-friendly. At its core, EconTrust is designed to empower individuals and families to take control of their finances,
+             offering a comprehensive platform to manage, monitor, and grow their savings effectively.</p>
+                <div>
+        </div>
+        </div>
+       
+        </>
+    )
+}
+export default About;

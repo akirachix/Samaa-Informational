6580477712d38822f399eeb922e7ece8d38c0e46
Zippy-Waluse <zippywaluse@gmail.com>
2024-07-05 16:58:40 +0300

    made changes

--- econtrust/src/Mission/index.js
+++ econtrust/src/Mission/index.js
@@ -1,9 +1,9 @@
 import './index.css';
 const Mission=()=>{
   return(
-             <div className='tbox'>
+             <div>
                <h1 className='mission-texts'> Our Mission</h1>
-               <p className='textpara'>Our primary objective is to support Rwandan tea farmers by improving their financial stability through
+               <p className='textparagraph'>Our primary objective is to support Rwandan tea farmers by improving their financial stability through
                 the utilization of Village Savings and Loan Associations (VSLAs). 
                 Specifically, we intend to enhance the security of their financial transactions, streamline administrative processes and provide digital savings opportunities. Our commitment lies in empowering established VSLAs by leveraging their unique benefits.</p>
            </div>

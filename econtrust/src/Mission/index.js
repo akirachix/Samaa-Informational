e1f151567ef90add722f74e97f188a306af8d864
Minu-Ochieng <minuochieng@gmail.com>
2024-07-11 18:36:52 +0300

    Added changes

--- econtrust/src/Mission/index.js
+++ econtrust/src/Mission/index.js
@@ -2,7 +2,7 @@ import './index.css';
 const Mission=()=>{
   return(
              <div>
-               <h1 className='mission-title'> Our Mission</h1>
+               <h1 className='mission-title'> OUR MISSION</h1>
                <p className='mission-text'>Our primary objective is to support Rwandan tea farmers by improving their financial stability through
                 the utilization of Village Savings and Loan Associations (VSLAs).
                 Specifically, we intend to enhance the security of their financial transactions, streamline administrative processes and provide digital savings opportunities. Our commitment lies in empowering established VSLAs by leveraging their unique benefits.</p>

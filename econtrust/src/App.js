705f6ae07bd4a8ae26b989f6784a694344f2b273
Minu-Ochieng <minuochieng@gmail.com>
2024-07-15 14:52:30 +0300

    Added media queries

--- econtrust/src/App.js
+++ econtrust/src/App.js
@@ -2,7 +2,6 @@
 import Services from "./Services";
 import Team from "./Team";
 import Vision from "./Vision";
-import Mission from "./Mission";
 import About from "./About";
 import Mission from "./Mission"
 

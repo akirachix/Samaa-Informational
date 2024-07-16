30c6898f7000fe913c671c45f6691287d2652686
Kur-Achan <achananyiethkur@gmail.com>
2024-07-16 11:16:34 +0300

    add changes

--- econtrust/src/Footer/index.js
+++ econtrust/src/Footer/index.js
@@ -11,31 +11,33 @@ const Footer = () => {
     <img className='icon1' src={twitter} />
     <img className='icon1' src={instagram}/>
     <img className='icon1' src={facebook}/>
+    {/* <p>{price}</p> */}
+
     </div>
 
     <br></br>
     <div className='attributes'>
     <p className='pro'><b>Features</b></p>
     <p>Savings</p>
-    <p className='lon'>Loans</p>
+    <p>Loans</p>
     <p>Resources & 
          Materials</p>
     </div>
-    <br></br>
+    {/* <br></br> */}
     <div className='firm'>
     <p className='comp'><b>Company</b></p>
     <p> Team</p>
-     <p className='us'>About Us</p>
+     <p>About Us</p>
     <p> Contact</p>
     </div>
-    <br></br>
+    
     <div className='valid'>
 <p><b className='ligal'>Legal</b></p>
     <p>Privacy Policy</p>
     <p>Terms of Use</p>
     <p>Cookies Policy</p>
     </div>
-    <br></br>
+   
    
     
 </div>
